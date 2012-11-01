4dbb50b2a64a213b08e5dc7109ae8dd89e904aed
YuFei Zhu <phil@mohc.net>
2012-11-01 13:49:01 +0000

    First complete version of simple email subscription wordpress plugin

---
+++ define.php
@@ -0,0 +1,7 @@
+<?php
+global $wpdb;
+define('SIMPLE_EMAIL_SUBSCRIBER_PLUGIN_DIR', plugin_dir_path(__FILE__));  //plugin dir
+define('SIMPLE_EMAIL_SUBSCRIBER_DB_NAME', $wpdb->prefix."email_subscription");
+define('SIMPLE_EMAIL_SUBSCRIBE_LOCATION', 'http://wordpress.org/extend/plugins/');
+define('SES_PLUGIN_VERSION', "1.0");
+?>
